4929ed1043c08ee1c7af2df29e1b6ff356bf7102
sypa34 <127894849+sypa34@users.noreply.github.com>
2024-06-21 11:02:02 -0400

    Test Fitbit Webhook Posts

--- processFitbitData.py
+++ processFitbitData.py
@@ -1,7 +1,10 @@
 import boto3
 import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 dynamodb_client = boto3.client('dynamodb')
 def lambda_handler(event, context):
-    print("Hello")
-    print("fuck you")
+    logger.info("Recieved Event:", json.dumps(event))
