ea9894232069596baba15bfe33f6ae32595de12f
sypa34 <127894849+sypa34@users.noreply.github.com>
2024-06-25 13:18:38 -0400

    Update processFitbitData.py

--- processFitbitData.py
+++ processFitbitData.py
@@ -58,7 +58,7 @@ def lambda_handler(event, context):
     client_secret_parameter = get_parameter("Fitbit_Client_Secret", True)
     refresh_token_parameter = get_parameter("Fitbit_Refresh_Token", True)
     token_response = refresh_access_token(client_id_parameter, client_secret_parameter, refresh_token_parameter)
-    logger.info("Token response: " + token_response)
+    logger.info("Token response: ", token_response)
     
     if "access_token" in token_response:
         access_token = token_response["access_token"]
