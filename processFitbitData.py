41d3fa20261cc1adb0ec536ecbdc1fc5e200030d
sypa34 <127894849+sypa34@users.noreply.github.com>
2024-07-08 12:14:45 -0400

    Update processFitbitData.py

--- processFitbitData.py
+++ processFitbitData.py
@@ -328,10 +328,21 @@ def add_data_dyanamodb(data):
     
     for item in transformed_data:
         try:
+            # Convert numerical values to Decimal
+            if 'breathing_rate' in item:
+                item['breathing_rate'] = Decimal(item['breathing_rate'])
+            elif 'temperature' in item:
+                item['temperature'] = Decimal(item['temperature'])
+            elif 'averageSpO2' in item:
+                item['averageSpO2'] = Decimal(item['averageSpO2'])
+                item['minSpO2'] = Decimal(item['minSpO2'])
+                item['maxSpO2'] = Decimal(item['maxSpO2'])
+            
             table.put_item(Item=item)
             logger.info(f"Item placed in DynamoDB Table: {item}")
         except Exception as e:
             logger.error(f'An error occured when attempting to place item in Table: {e}')
+
 
 
 def lambda_handler(event, context):
